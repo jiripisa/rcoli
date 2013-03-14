6b723cd1ac94b9e9da9f35a23fdf80412a2ee9ee
Jiri Pisa <jiri.pisa@jetminds.com>
2013-03-14 10:42:37 -0700

    Support for helps

---
+++ lib/rcoli/help.rb
@@ -0,0 +1,17 @@
+require 'erb'
+
+module RCoLi
+
+  module Help
+  
+    def help
+      puts template 'help'
+    end
+
+    def template(name)
+      ERB.new(File.read(File.join(File.dirname(__FILE__), 'templates', "#{name}.erb")), nil, '-').result binding
+    end
+  
+  end
+
+end
