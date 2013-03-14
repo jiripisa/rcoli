7cfeb4c3bfaff538ac4cc4db3262f56910187056
Jiri Pisa <jiri.pisa@jetminds.com>
2013-03-14 12:09:24 -0700

    Added support for flags and options

--- lib/rcoli/model.rb
+++ lib/rcoli/model.rb
@@ -2,11 +2,24 @@ module RCoLi
   
   module CommandContainer
     def command(name, &block)
-      cmnd = RCoLi::Command.new(name)
-      # block.call(cmnd)  
-      cmnd.instance_eval &block
-      (@commands ||= []) << cmnd
+      obj = Command.new(name)
+      block.call(obj)  
+      # cmnd.instance_eval &block
+      (@commands ||= []) << obj
     end
+    
+    def switch(names, &block)
+      obj = Switch.new(names)
+      block.call(obj) if block_given?  
+      (@options ||= []) << obj
+    end
+    
+    def flag(names, &block)
+      obj = Flag.new(names)
+      block.call(obj) if block_given? 
+      (@options ||= []) << obj
+    end
+    
   end
   
   module Program
@@ -21,19 +34,38 @@ module RCoLi
     
   end
   
+  class Option
+    
+    extend RCoLi::Properties     
+
+    setter :description
+    
+    def initialize(names)
+      @s_name = names[:short]
+      @l_name = names[:long]
+    end
+    
+  end
+  
+  class Switch < Option
+  end
+  
+  class Flag < Option
+    
+    setter :default_value
+    setter :arg_name
+    
+  end
+    
   class Command
     
     extend RCoLi::Properties 
 
-    attr_reader :commands
-
-    setter :name
     setter :summary
     setter :description
     setter :syntax
     
     def initialize(name)
-      @commands = []
       @name = name
     end
     
